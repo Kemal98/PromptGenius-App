3c9f7a354849f99865420e8fbe827a573a0abe28
Kemal98 <mokskemal@gmail.com>
2023-05-28 01:43:14 +0200

    update

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className="mt-16 prompt_layout">
+    <div className="sm:mt-12 prompt_layout">
       {data.map((post) => (
         <PromptCard key={post.id} post={post} handleTagClick={handleTagClick} />
       ))}
